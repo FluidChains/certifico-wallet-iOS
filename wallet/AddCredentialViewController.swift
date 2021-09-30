84d1af943d7515615fa0dd2d97e8316a2f282ac4
checho1989 <cheverdolaga@hotmail.com>
2021-09-30 15:16:01 -0400

    Update iTunes URLs

--- wallet/AddCredentialViewController.swift
+++ wallet/AddCredentialViewController.swift
@@ -175,7 +175,7 @@ class AddCredentialViewController: UIViewController, UIDocumentPickerDelegate {
         let okayButton = DialogButton(frame: .zero)
         okayButton.setTitle(Localizations.Okay, for: .normal)
         okayButton.onTouchUpInside {
-            let url = URL(string: "itms://itunes.apple.com/us/app/certifico/id1543769590")!
+            let url = URL(string: "itms://itunes.apple.com/us/app/chertero/id1543769590")!
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
             alert.dismiss(animated: false, completion: nil)
             Logger.main.tag(tag).debug("tapped update_app with link: \(url)")
